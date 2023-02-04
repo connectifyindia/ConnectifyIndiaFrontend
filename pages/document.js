6cb39132d579f14dc4e9848f6c82a49c7ebad989
connectifyindia <123191836+connectifyindia@users.noreply.github.com>
2023-02-04 15:16:06 +0530

    Update document.js

--- pages/document.js
+++ pages/document.js
@@ -16,31 +16,31 @@ const Document = () => {
             <div className='w-[80%] mx-auto my-28'>
                 <div className='text-4xl  my-5 font-bold capitalize text-center mb-20'>Documents</div>
                 <div className='flex justify-center items-center gap-4 flex-wrap'>
-                    <Link href="https://connectifyindia.azurewebsites.net/carrier-doc">
+                    <Link href="https://connectifyglobal.azurewebsites.net/carrier-doc">
                     <div className='flex flex-col justify-center items-center md:w-60 w-full h-72 bg-indigo-400 gap-4'>
                         <div className="text-8xl text-sky-400"><SiGotomeeting /></div>
                         <div className="text-lg capitalize">Carrier Document</div>
                     </div>
                     </Link>
-                    <Link href="https://connectifyindia.azurewebsites.net/legal-doc">
+                    <Link href="https://connectifyglobal.azurewebsites.net/legal-doc">
                     <div className='flex flex-col justify-center items-center md:w-60 w-full h-72 bg-indigo-400 gap-4'>
                         <div className="text-8xl text-sky-400"><MdBusinessCenter /></div>
                         <div className="text-lg capitalize">Legal Document</div>
                     </div>
                     </Link>
-                    <Link href="https://connectifyindia.azurewebsites.net/ngo-doc">
+                    <Link href="https://connectifyglobal.azurewebsites.net/ngo-doc">
                     <div className='flex flex-col justify-center items-center md:w-60 w-full h-72 bg-indigo-400 gap-4'>
                         <div className="text-8xl text-sky-400"><FcDocument /></div>
                         <div className="text-lg capitalize">Ngo Document</div>
                     </div>
                     </Link>
-                   <Link href="https://connectifyindia.azurewebsites.net/project-doc">
+                   <Link href="https://connectifyglobal.azurewebsites.net/project-doc">
                    <div className='flex flex-col justify-center items-center md:w-60 w-full h-72 bg-indigo-400 gap-4'>
                         <div className="text-8xl text-sky-400"><AiOutlineTrademarkCircle /></div>
                         <div className="text-lg capitalize">Project Document</div>
                     </div>
                    </Link>
-                   <Link href="https://connectifyindia.azurewebsites.net/all-doc">
+                   <Link href="https://connectifyglobal.azurewebsites.net/all-doc">
                    <div className='flex flex-col justify-center items-center md:w-60 w-full h-72 bg-indigo-400 gap-4'>
                         <div className="text-8xl text-sky-400"><AiOutlineTrademarkCircle /></div>
                         <div className="text-lg capitalize px-4">See All Document In All Cattegory</div>
